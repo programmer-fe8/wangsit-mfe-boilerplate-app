564dc93e53ed97c1ef35961edd63084f5e12b69a
Zain Kurnia <programmer-fe3@qwertysystem.net>
2024-10-09 12:19:53 +0700

    chore: update wangsvue version and implement new way how to use presets

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { inject } from 'vue';
 import { Toast } from 'wangsvue';
+
+const preset = inject('preset');
+
+console.log(preset.tabmenu);
 </script>
 
 <template>
--- src/utils/registerComponents.util.ts
+++ src/utils/registerComponents.util.ts
@@ -1,16 +1,20 @@
 import { App } from 'vue';
-import { Tooltip, WangsVue, Preset, ToastService } from 'wangsvue';
+import { Tooltip, WangsVue, ToastService } from 'wangsvue';
+import { wangsvue } from 'wangsvue-presets';
 
 import router from '@/router';
 
 import '@/assets/css/main.css';
 import 'wangsvue/style.css';
+import 'wangsvue-presets/wangsvue/style.css';
 
 const registerComponents = (app: App): void => {
   app.use(WangsVue, {
     unstyled: true,
-    pt: Preset,
+    pt: wangsvue,
   });
+
+  app.provide('preset', wangsvue);
 
   app.use(router);
   app.use(ToastService);
