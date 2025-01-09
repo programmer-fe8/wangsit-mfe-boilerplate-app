60befaca41ee76c1bb05758a272354b4a95606c9
Ibrahim Gunawan <programmer-fe8@qwertysystem.net>
2025-01-09 10:07:38 +0700

    fix: resolve todo

--- src/views/DetailAssetsView.vue
+++ src/views/DetailAssetsView.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import { useBreadcrumbStore } from '@/store';
-import { Image } from 'wangsvue';
+
 import { BreadcrumbMenu } from 'wangsvue/components/breadcrumb/Breadcrumb.vue';
+import { Image } from 'wangsvue';
+import { useBreadcrumbStore } from '@/store';
 
 const { setBreadcrumbs } = useBreadcrumbStore();
 const imagePublicUrl =
   'https://images.unsplash.com/photo-1723556146809-2dcb7c1e0bf1?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
 const date = new Date();
+
+const assetInfo = {
+  brand: {
+    label: 'Brand',
+    value: 'Daihatsu',
+  },
+  model: {
+    label: 'Model',
+    value: 'Grand Max PU',
+  },
+  category: {
+    label: 'Category',
+    value: 'Kendaraan > Mobil > Mobil operasional',
+  },
+  group: {
+    label: 'Group',
+    value: 'Kacab 1 > Gedung A > Div Pengadaan',
+  },
+};
 
 const menus: BreadcrumbMenu[] = [
   {
@@ -26,30 +47,18 @@ onMounted(() => {
   </div>
   <div class="flex mt-5">
     <Image :src="imagePublicUrl" class="object-cover w-40 h-40" />
+
     <div class="flex flex-col ml-6">
       <h3 class="">General Information</h3>
       <div class="grid grid-cols-2">
-        <!-- TODO: Ini sebaiknya pake v-for, yang loop objek -->
-        <ul class="text-slate-600 mt-2 mr-10">
-          Brand
-          <li class="text-base text-black">Daihatsu</li>
-        </ul>
-        <ul class="text-slate-600 mt-2 mr-10">
-          Model/Type
-          <li class="text-base text-black">Grand Max PU</li>
-        </ul>
-        <ul class="text-slate-600 mt-2 mr-10">
-          Category
-          <li class="text-base text-black">
-            Kendaraan > Mobil > Mobil operasional
-          </li>
-        </ul>
-        <ul class="text-slate-600 mt-2 mr-10">
-          Group
-          <li class="text-base text-black">
-            Kacab 1 > Gedung A > Div Pengadaan
-          </li>
-        </ul>
+        <div :key="asset.value" v-for="asset in assetInfo">
+          <ul class="text-slate-600 mt-2 mr-10">
+            {{
+              asset.label
+            }}
+            <li class="text-base text-black">{{ asset.value }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
